f60e59863eb226abbcd6a32e0dad1e50a9462247
deltager <github@software.unf.dk>
2017-07-06 13:14:24 +0200

    Initial Commit. With errors and everything.

---
+++ app/src/main/java/com/example/deltager/pointlessclicker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.deltager.pointlessclicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        long clickCounter = 0;
+        Button red;
+
+
+
+
+
+        red = findViewById(redbutt);
+
+
+
+    }
+}
