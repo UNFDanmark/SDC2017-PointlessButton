d306cba3a3b309ab794e3e22e2ac50380cf5143a
deltager <github@software.unf.dk>
2017-07-07 10:54:43 +0200

    Moved Toast added animation to RedButt

--- app/src/main/java/com/example/deltager/pointlessclicker/MainActivity.java
+++ app/src/main/java/com/example/deltager/pointlessclicker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.deltager.pointlessclicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +14,6 @@ import java.util.Random;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
     private long clickCounter = 0;  // Variables
     public TextView counter;
     public ImageButton red;
@@ -21,23 +21,27 @@ public class MainActivity extends AppCompatActivity {
     public ImageButton menu;
     public EditText addAdjective;
     public EditText addNickName;
-    public Toast toast;
-    public double chance = 1;
 
-    insultGenerator insult = new insultGenerator();
+    Toaster toaster = new Toaster();
 
     // ved app start
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         red = (ImageButton) findViewById(R.id.redButt);     // Path to layout objects
+
         mute = (ImageButton) findViewById(R.id.soundMute);
         menu = (ImageButton) findViewById(R.id.menuButt);
         counter = (TextView) findViewById(R.id.clickText);
         addNickName = (EditText) findViewById(R.id.addNickName);
         addAdjective = (EditText) findViewById(R.id.addAdjective);
+    }
+
+    //click counter
+    public void click(View view) {
+        updateClickCounter();
+        toaster.makeToast(this);
     }
 
     public void updateClickCounter()    // UpdateClickCounter
@@ -50,44 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public void interactMenu() {
 
     }
-
-    public int genRandomNum()   // Generate random number
-    {
-        int randomNum = (int) (Math.random() * 1000000);
-        return randomNum;
-    }
-
-    public void click(View view)    // Click counter
-    {
-        updateClickCounter();
-        makeToast();
-    }
-
-
-    public void makeToast() {
-        String toastText = insult.getRandomInsult();
-
-        if (genRandomNum() < 100 * chance) {
-            if (toast != null) {
-               toast.cancel();
-        }
-        toast = Toast.makeText(this, toastText, Toast.LENGTH_LONG);
-            toast.show();
-            toast.show();
-            toast.show();
-            toast.show();
-            chance = 1;
-
-        } else {
-            chance = chance * 1.15;
-            System.out.println(chance);
-        }
-
-    }
-
 
 
 }
---
+++ app/src/main/java/com/example/deltager/pointlessclicker/Toaster.java
@@ -0,0 +1,46 @@
+package com.example.deltager.pointlessclicker;
+
+import android.widget.Toast;
+
+/**
+ * Created by deltager on 07-07-17.
+ */
+
+public class Toaster {
+
+
+    insultGenerator insult = new insultGenerator();
+    public double chance = 1;
+    public Toast toast;
+
+
+
+    public int genRandomNum()   // Generate random number
+    {
+        int randomNum = (int) (Math.random() * 1000000);
+        return randomNum;
+    }
+
+
+
+    public void makeToast(MainActivity activity) {
+        String toastText = insult.getRandomInsult();
+
+        if (genRandomNum() < 100 * chance) {
+            if (toast != null) {
+                toast.cancel();
+            }
+            toast = Toast.makeText(activity, toastText, Toast.LENGTH_LONG);
+            toast.show();
+            toast.show();
+            toast.show();
+            toast.show();
+            chance = 1;
+
+        } else {
+            chance = chance * 1.15;
+            System.out.println(chance);
+        }
+
+    }
+}
